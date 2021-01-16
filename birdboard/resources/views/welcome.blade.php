8a2188038e3ebf81214ca8f3939aca6dc868cf8a
=Peter Sannan <=peter.sannan@gmail.com>
2021-01-16 13:09:56 +0200

    ssaa

--- birdboard/resources/views/welcome.blade.php
+++ birdboard/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laavel1111sdsdccdfsseee
+                    Laavel1111sdsdccdfsseeeaaa
                 </div>a
 
                 <div class="links">
