adb44dc1cced9ae9c17ff3b4911c00269d24756d
=Peter Sannan <=peter.sannan@gmail.com>
2021-01-16 12:46:20 +0200

    ssaa

--- birdboard/resources/views/welcome.blade.php
+++ birdboard/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel1111sdsdccd
+                    Laravel1111sdsdccdf
                 </div>
 
                 <div class="links">
